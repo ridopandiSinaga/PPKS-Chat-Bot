e183c9cd12a4c51b4cf535c59e767d8d2f6a4aef
Ridopandi Sinaga <89272004+ridopandiSinaga@users.noreply.github.com>
2024-12-05 04:51:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -529,4 +529,12 @@ if prompt:
             st.error("Terjadi limit penggunaan, silakan coba lagi nanti.")
         else:
             st.error(f"Terjadi error: {e}")
-        
+
+# Hide footer logo
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
